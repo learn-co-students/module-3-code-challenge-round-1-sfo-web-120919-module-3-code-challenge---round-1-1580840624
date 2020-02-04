183c5a304adf1748118e837f5a257f1d5dff012a
Kuddleman <edl1155@gmail.com>
2020-02-04 12:03:13 -0800

    Donny's code challenge

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   console.log('%c DOM Content Loaded and Parsed!', 'color: magenta')
 
-  let imageId = 1 //Enter the id from the fetched image here
+  let imageId = 4470
 
   const imageURL = `https://randopic.herokuapp.com/images/${imageId}`
 
@@ -9,4 +9,73 @@ document.addEventListener('DOMContentLoaded', () => {
 
   const commentsURL = `https://randopic.herokuapp.com/comments/`
 
+  getImage()
+
+  //increaseLikes()
+
+  createComment()
+
+  console.log("DOM is loaded")
+
 })
+
+//https://randopic.herokuapp.com/images/4470
+
+function getImage() {
+  fetch('https://randopic.herokuapp.com/images/4470')
+  .then(resp => resp.json())
+  .then( myImage => {
+     const img = document.querySelector('#image');
+     const name = document.querySelector('#name');
+     const likeCount = document.querySelector('#likes');
+
+     const commentsUl = document.querySelector('#comments');
+    
+
+     img.src = myImage.url;
+     name.textContent = myImage.name;
+     likeCount.textContent = myImage.like_count;
+     commentsUl.innerHTML = `<li>${myImage.comments[0]["content"]}<li>`
+     
+     
+  })
+}
+
+
+
+//submit post request upon submitting a comment
+
+
+function createComment () {
+  const commentUl = document.querySelector('#comments')
+  
+  const commentForm = document.querySelector('#comment_form')
+  
+  commentForm.addEventListener('submit', function(event){
+    event.preventDefault;
+
+    //get input values
+    const commentText = document.querySelector('#comment_input').value 
+    //const myImageUrl = document.querySelector('#image').src
+
+    const data = {
+      
+           };
+    fetch('https://randopic.herokuapp.com/comments'{
+     method: "POST",
+     body: JSON.stringify(data),
+       headers: { "Content-Type": "application/json" }
+    })
+    .then(resp => resp.json())  
+    .then(newComment => {
+        const commentForm = document.querySelector('#comment_form')
+        commentForm.innerHTML += <li>${newComment}</li>
+            });   
+    
+  })
+  //inside event listener above
+
+
+
+}
+
